ca375e89958d320fcaef0c9e362da353730d5338
tenkesnet <rbardo@gmail.com>
2023-05-06 09:29:59 +0200

    változó nevezéktan

--- ex3/src/pelda.js
+++ ex3/src/pelda.js
@@ -1,4 +1,12 @@
 // 1,2,567,-3,-78
 // 1.6, 0.5,-3.6
 // true, false
-console.log('hello világ!!!!!!!!!');
+//camelCase motorTerfogatSzam 
+//PascalCase MotorTerfogat
+//underscore_case motor_terfogat
+var terfogat;
+var nev = "Robi";
+terfogat = 50;
+console.log(nev);
+nev = "Józsi";
+console.log(terfogat);
--- ex3/src/pelda.ts
+++ ex3/src/pelda.ts
@@ -1,4 +1,14 @@
 // 1,2,567,-3,-78
 // 1.6, 0.5,-3.6
 // true, false
-console.log('hello világ!!!!!!!!!');
+//camelCase motorTerfogatSzam 
+//PascalCase MotorTerfogat
+//underscore_case motor_terfogat
+let terfogat: number;
+let nev: String = "Robi"
+terfogat = 50;
+
+console.log(nev);
+nev = "Józsi"
+
+console.log(terfogat);
