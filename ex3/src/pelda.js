817028683f32d9b7b0c18c61412edb7e1a44179b
tenkesnet <rbardo@gmail.com>
2023-05-06 12:08:52 +0200

    Osztály alapok

--- ex3/src/pelda.js
+++ ex3/src/pelda.js
@@ -10,6 +10,38 @@ terfogat = 100;
 var nev = "Robi";
 var terfogatLista = [23, 78, 56.8];
 var kutya = terfogatLista;
+function osszead(tomb) {
+    var result = 0;
+    for (var _i = 0, tomb_1 = tomb; _i < tomb_1.length; _i++) {
+        var item = tomb_1[_i];
+        result = result + item;
+    }
+    //console.log(item);
+    return result;
+}
+var Utazas = /** @class */ (function () {
+    function Utazas() {
+        this.cel = "";
+        this.tavolsag = 0;
+    }
+    return Utazas;
+}());
+var Alkalmazott = /** @class */ (function () {
+    function Alkalmazott() {
+        this.nev = "";
+        this.utazasai = [];
+    }
+    Alkalmazott.prototype.setNev = function (nev) {
+        this.nev = nev;
+    };
+    return Alkalmazott;
+}());
+var robi = new Alkalmazott();
+var peti = new Alkalmazott();
+robi.setNev("Bárdonicsek Róbert");
+peti.
+    peti.setNev("lkjlééj");
+robi.utazasai.push(new Utazas());
 var alkalmazott = {
     "nev": "Robi",
     "utazasai": [
@@ -30,4 +62,4 @@ kutya[0] = 100;
 nev = "Józsi";
 // console.log("terfogatLista:" + terfogatLista);
 // console.log("kutya:" + kutya);
-console.log(alkalmazott.utazasai[0].utazasiIroda);
+console.log(osszead(terfogatLista));
--- ex3/src/pelda.ts
+++ ex3/src/pelda.ts
@@ -8,8 +8,40 @@ let terfogat: number = 50;
 let uvegTerfogat: number = terfogat;
 terfogat = 100;
 let nev: String = "Robi"
+
 let terfogatLista: number[] = [23, 78, 56.8]
 let kutya = terfogatLista
+
+
+function osszead(tomb: number[]): number {
+    let result: number = 0
+    for (let item of tomb) {
+        result = result + item;
+    }
+    //console.log(item);
+    return result;
+}
+
+class Utazas {
+    cel: String = ""
+    tavolsag: number = 0
+}
+
+class Alkalmazott {
+    protected nev: String = ""
+    utazasai: Utazas[] = []
+
+    setNev(nev: string): void {
+        this.nev = nev;
+    }
+}
+
+let robi: Alkalmazott = new Alkalmazott();
+let peti: Alkalmazott = new Alkalmazott();
+robi.setNev("Bárdonicsek Róbert")
+peti.
+    peti.setNev("lkjlééj")
+robi.utazasai.push(new Utazas());
 
 let alkalmazott = {
     "nev": "Robi",
@@ -33,4 +65,4 @@ nev = "Józsi"
 
 // console.log("terfogatLista:" + terfogatLista);
 // console.log("kutya:" + kutya);
-console.log(alkalmazott.utazasai[0].utazasiIroda);
+console.log(osszead(terfogatLista));
