57e70ca1db49e37f0e19eaf5a4ddf8d5fed4edd1
tenkesnet <rbardo@gmail.com>
2023-05-20 11:51:08 +0200

    Primitívek

--- ex5/alapok/src/main/java/org/pallas/Macska.java
+++ ex5/alapok/src/main/java/org/pallas/Macska.java
@@ -2,8 +2,18 @@ package org.pallas;
 
 public class Macska {
     public static void main(String[] parameterek) {
+        int haziMacskaSzam=5;
+        short terfogat = (short) -42769;
+        float a = 0.4f;
+        double b = 5.6d;
+        char c= 'n';
+        boolean isEnable=true;
 
-        System.out.print("Hello Macska!!\n");
-        System.out.print("Hello Macska!!\n");
+        String text = "hjhkhkl";
+
+        System.out.println(haziMacskaSzam);
+        System.out.println(c);
+        System.out.println(terfogat);
+        System.out.println("Hello Macska!!");
     }
 }
