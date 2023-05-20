85395da08182af8fb96a8ef644a5a969d776fe36
tenkesnet <rbardo@gmail.com>
2023-05-20 12:03:24 +0200

    Üres osztály és a stringek

--- ex5/alapok/src/main/java/org/pallas/Macska.java
+++ ex5/alapok/src/main/java/org/pallas/Macska.java
@@ -1,19 +1,12 @@
 package org.pallas;
 
 public class Macska {
+    int bolhaSzam;
     public static void main(String[] parameterek) {
-        int haziMacskaSzam=5;
-        short terfogat = (short) -42769;
-        float a = 0.4f;
-        double b = 5.6d;
-        char c= 'n';
-        boolean isEnable=true;
 
-        String text = "hjhkhkl";
-
-        System.out.println(haziMacskaSzam);
-        System.out.println(c);
-        System.out.println(terfogat);
-        System.out.println("Hello Macska!!");
+        Macska cirmi = new Macska();
+        String text ="Józsi";
+        String text2 ="Józsi";
+        //System.out.println("Hello Macska!!");
     }
 }
