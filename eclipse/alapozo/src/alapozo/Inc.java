7cb32e18b89d38251f9511fb6ca1fe5edad5158d
tenkesnet <rbardo@gmail.com>
2023-06-10 10:54:37 +0200

    Filekezelés

---
+++ eclipse/alapozo/src/alapozo/Inc.java
@@ -0,0 +1,13 @@
+package alapozo;
+
+public class Inc {
+	private int value;
+
+	public void inc(int incValue) {
+		value += incValue;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
--- eclipse/alapozo/src/alapozo/Pelda1.java
+++ eclipse/alapozo/src/alapozo/Pelda1.java
@@ -1,16 +1,8 @@
 package alapozo;
 
-class Inc{
-	private int value;
-	
-	public void inc(int incValue) {
-		value+=incValue;
-	}
-	
-	public int getValue() {
-		return value;
-	}
-}
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Pelda1 {
 	String nev;
@@ -19,18 +11,26 @@ public class Pelda1 {
 		vizsgalando.inc(1);
 		return vizsgalando.getValue()<hatarertek;
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		String[] nevek = { "Robi", "Gizi", "Jani", "a","b" };
 		// String[] nevek = { "Robi" };
 		
 		Inc jref=new Inc();
 		Inc iref=new Inc();
+		
 		Pelda1 pelda= new Pelda1();
-		Pelda1 pelda2 = new Pelda1();
+		Pelda1 szamar = new Pelda1();
+		//System.out.println(szamar.nev.length());
+		
+		BufferedReader in;
+		
+		in = new BufferedReader(new FileReader("c:\home\bardo\repo\pallas\pallas146\eclipse\alapozo\src\alapozo\birka.txt"));	
+		
+        
 		pelda.nev="valami";
-		pelda2.nev="valami";
-		pelda2.nev="valami2";
+		szamar.nev="valami";
+		szamar.nev="valami2";
 		for(;(pelda.vizsgal(jref, nevek.length) && pelda.vizsgal(iref, nevek.length));) {
 			System.out.println(nevek[jref.getValue()]);
 		}
