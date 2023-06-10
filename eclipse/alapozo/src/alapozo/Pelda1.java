c465c5597323a81201583bd9888653939f64037b
tenkesnet <rbardo@gmail.com>
2023-06-10 11:52:15 +0200

    Exception kezelés

--- eclipse/alapozo/src/alapozo/Pelda1.java
+++ eclipse/alapozo/src/alapozo/Pelda1.java
@@ -3,39 +3,51 @@ package alapozo;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Pelda1 {
 	String nev;
-	public boolean vizsgal(Inc vizsgalando,int hatarertek) {
-		//System.out.println("Vizsgalat:  "+vizsgalando.getValue()+"<"+hatarertek);
+
+	public boolean vizsgal(Inc vizsgalando, int hatarertek) {
+		// System.out.println("Vizsgalat: "+vizsgalando.getValue()+"<"+hatarertek);
 		vizsgalando.inc(1);
-		return vizsgalando.getValue()<hatarertek;
+		return vizsgalando.getValue() < hatarertek;
 	}
+
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		String[] nevek = { "Robi", "Gizi", "Jani", "a","b" };
+		String[] nevek = { "Robi", "Gizi", "Jani", "a", "b" };
 		// String[] nevek = { "Robi" };
-		
-		Inc jref=new Inc();
-		Inc iref=new Inc();
-		
-		Pelda1 pelda= new Pelda1();
+
+		Inc jref = new Inc();
+		Inc iref = new Inc();
+
+		Pelda1 pelda = new Pelda1();
 		Pelda1 szamar = new Pelda1();
-		//System.out.println(szamar.nev.length());
-		
-		BufferedReader in;
-		
-		in = new BufferedReader(new FileReader("c:\home\bardo\repo\pallas\pallas146\eclipse\alapozo\src\alapozo\birka.txt"));	
-		
-        
-		pelda.nev="valami";
-		szamar.nev="valami";
-		szamar.nev="valami2";
-		for(;(pelda.vizsgal(jref, nevek.length) && pelda.vizsgal(iref, nevek.length));) {
-			System.out.println(nevek[jref.getValue()]);
+		try {
+			System.out.println(szamar.nev.length());
+		} catch (NullPointerException e) {
+			System.out.println("bakker vétettél!");
+			System.exit(0);
 		}
 		
-		
+		BufferedReader in;
+
+		String fileName = "birka.txt";
+		try {
+			in = new BufferedReader(new FileReader(fileName));
+		} catch (IOException e) {
+			System.out.println("nemtudtam megnyitni a " + fileName + "-t filet!");
+			System.exit(0);
+		}
+
+		pelda.nev = "valami";
+		szamar.nev = "valami";
+		szamar.nev = "valami2";
+		for (; (pelda.vizsgal(jref, nevek.length) && pelda.vizsgal(iref, nevek.length));) {
+			System.out.println(nevek[jref.getValue()]);
+		}
+
 //		if (nevek.length >= 5) {
 //			System.out.println("A nevek tömb hossza nagyobb vagy egyenlő 5");
 //		} else if (nevek.length > 2) {
