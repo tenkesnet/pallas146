e116226436ad925dfbc1e3893bbe1ca743405c36
tenkesnet <rbardo@gmail.com>
2023-06-24 04:52:05 +0200

    Mark hazi Beragadt fileok

---
+++ eclipse/markocs_mate_warrior/src/Util.java
@@ -0,0 +1,8 @@
+public class Util {
+    private String name;
+
+    public static int random(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+}
--- hazik/klimaj_mark/warrior2/src/jatek/Jatek.java
+++ hazik/klimaj_mark/warrior2/src/jatek/Jatek.java
@@ -3,30 +3,30 @@ package jatek;
 public class Jatek {
 
 	public static void main(String[] args) {
-		Harcos karakter = new Harcos(350, 50.51515414141, "Geza", 18);
-		Vadasz karakter1 = new Vadasz(200, 72.62525252414214, "Jani", 23);
-		while (karakter.eletEro > 0.0 && karakter1.eletEro > 0.0) {
+		Harcos karakter1 = new Harcos(350, 50.51515414141, "Geza", 18);
+		Vadasz karakter2 = new Vadasz(200, 72.62525252414214, "Jani", 23);
+		while (karakter1.eletEro > 0.0 && karakter2.eletEro > 0.0) {
 
-			karakter.Harcol(karakter1);
+			karakter1.Harcol(karakter2);
 
-			if (karakter.eletEro < 0.0) {
+			if (karakter1.eletEro < 0.0) {
+				System.out.println("A " + karakter2.nev + " a (" + karakter2.className + ") nyert");
+			}
+			if (karakter2.eletEro < 0.0) {
 				System.out.println("A " + karakter1.nev + " a (" + karakter1.className + ") nyert");
 			}
-			if (karakter1.eletEro < 0.0) {
-				System.out.println("A " + karakter.nev + " a (" + karakter.className + ") nyert");
-			}
 			// javitando
-			if (karakter1.eletEro < karakter1.eletEroMax / 2 && !karakter1.isBuffed() && karakter.eletEro>0.0) {
-				karakter1.Buff();
-				karakter1.sebzes = karakter1.Orvloves();
-			} else if (karakter1.isBuffed()) {
-				karakter1.sebzes = karakter1.Orvloves();
+			if (karakter2.eletEro < karakter2.eletEroMax / 2 && !karakter2.isBuffed() && karakter1.eletEro>0.0) {
+				karakter2.Buff();
+				karakter2.sebzes = karakter2.Orvloves();
+			} else if (karakter2.isBuffed()) {
+				karakter2.sebzes = karakter2.Orvloves();
 			}
-			karakter.checkHP();
 			karakter1.checkHP();
-			karakter.flee();
+			karakter2.checkHP();
+			karakter1.flee();
 		}
-		karakter.printFinalRound();
+		karakter1.printFinalRound();
 
 	}
 }
