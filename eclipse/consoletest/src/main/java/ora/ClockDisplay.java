1589f70e3501bfc6e52c786c5f14c8c51acb976d
tenkesnet <rbardo@gmail.com>
2023-07-22 13:47:14 +0200

    Vége az Ora osztál yimplementálásnak

--- eclipse/consoletest/src/main/java/ora/ClockDisplay.java
+++ eclipse/consoletest/src/main/java/ora/ClockDisplay.java
@@ -1,4 +1,35 @@
 package ora;
 
 public class ClockDisplay {
+    private final NumberDisplay hours;
+    private final NumberDisplay minutes;
+    private String displayString;
+
+    public ClockDisplay(){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        updateDisplay();
+    }
+
+    public void updateDisplay(){
+        displayString = hours.getDisplayValue()+":"+minutes.getDisplayValue();
+    }
+
+    public String getTime(){
+        return displayString;
+    }
+
+    public void setTime(int hour, int minutes){
+        hours.setValue(hour);
+        this.minutes.setValue(minutes);
+        updateDisplay();
+    }
+
+    public void timeTick(){
+        minutes.increment();
+        if(minutes.getValue() == 0){
+            hours.increment();
+        }
+        updateDisplay();
+    }
 }
