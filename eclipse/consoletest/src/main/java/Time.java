c9bc021830e0dc0ea7f605747ef28f2ab6f94150
tenkesnet <rbardo@gmail.com>
2023-07-22 09:06:02 +0200

    Consoletest

---
+++ eclipse/consoletest/src/main/java/Time.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Time {
+    public static void main(String... args) throws InterruptedException {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        while(true) {
+            System.out.printf("\r%s", sdf.format(new Date()));
+            Thread.sleep(1000);
+        }
+    }
+}
