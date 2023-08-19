32314e46439c417c61f9f4c6fae68e246acb4935
tenkesnet <rbardo@gmail.com>
2023-08-19 10:33:15 +0200

    Dolgozok lista feltöltése

--- java_alapok/oopalapok/src/alapozo/Dolgozo.java
+++ java_alapok/oopalapok/src/alapozo/Dolgozo.java
@@ -1,4 +1,106 @@
 package alapozo;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Dolgozo {
+    int azon;
+    String firstName;
+    String lastName;
+    String gender;
+    String country;
+    int age;
+
+    Date date;
+
+    int id;
+
+    public Dolgozo(int azonosito, String firstName,
+                   String lastName, String gender,
+                   String country, int age, Date date, int id){
+        this.azon=azonosito;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.gender=gender;
+        this.country = country;
+        this.age=age;
+        this.date=date;
+        this.id=id;
+    }
+
+
+
+    public int getAzon() {
+        return azon;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Dolgozo{" +
+                "azon=" + azon +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", country='" + country + '\'' +
+                ", age=" + age +
+                ", date=" + date +
+                ", id=" + id +
+                '}';
+    }
 }
--- java_alapok/oopalapok/src/alapozo/StreamExamples.java
+++ java_alapok/oopalapok/src/alapozo/StreamExamples.java
@@ -3,6 +3,9 @@ package alapozo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -15,52 +18,68 @@ public class StreamExamples {
     List<Integer> ints = Arrays.asList(5, 3, 7, 8, 2);
     List<Harcos> harcosok = Arrays.asList(
             new Harcos("Pisti", 20, 2)
-            ,new Harcos("Jani", 18, 2)
-            ,new Harcos("Géza", 25, 3)
-            ,new Harcos("Kata", 20, 1)
+            , new Harcos("Jani", 18, 2)
+            , new Harcos("Géza", 25, 3)
+            , new Harcos("Kata", 20, 1)
     );
-    List<Dolgozo> dolgozok;
+    List<Dolgozo> dolgozok=new ArrayList<>();
+
     public StreamExamples() {
     }
-    public void reduce(){
+
+    public void reduce() {
 
         List<String> pGyumik = fruits.stream().filter((gyumolcs) -> {
             return gyumolcs.length() > 5;
         }).collect(Collectors.toList());
-        Stream<Harcos> szurtStream=harcosok.stream()
+        Stream<Harcos> szurtStream = harcosok.stream()
                 //.limit(1)
-                .filter(h -> h.getSebzesiEro() >0)
-                .peek(h -> h.setEletEro(h.getEletEro()+100));
+                .filter(h -> h.getSebzesiEro() > 0)
+                .peek(h -> h.setEletEro(h.getEletEro() + 100));
 
-        harcosok.forEach(x-> System.out.println(x));
+        harcosok.forEach(x -> System.out.println(x));
         szurtStream.collect(Collectors.toList());
         System.out.println("------------------");
         //az accumulator az első elemet kapja meg az item ha a size>1 akkor a másodiktól iterál
         //ha size==1 akkor az accumulator és az item is az első elem értékét veszi fel.
         Optional<Harcos> result = harcosok.stream()
                 .reduce((accumlator, item) -> {
-                    accumlator = new Harcos("",0,0);
-                    accumlator.setEletEro(accumlator.getEletEro()+ item.getEletEro());
+                    accumlator = new Harcos("", 0, 0);
+                    accumlator.setEletEro(accumlator.getEletEro() + item.getEletEro());
                     return accumlator;
-                } );
-        result.ifPresent(x-> System.out.println(x));
+                });
+        result.ifPresent(x -> System.out.println(x));
         System.out.println("");
     }
-    public void dolgozokBeolvasasa(){
+
+    public void dolgozokBeolvasasa() {
         try {
-            List<String> lines =Files.readAllLines(Path.of("file_example_CSV_5000.csv"));
-            Sorszam sorszam= new Sorszam(1);
-            lines.stream().forEach(x -> {
-                if(sorszam.get()!=1) {
-                    String[] fields = x.split(",");
+            List<String> lines = Files.readAllLines(Path.of("file_example_CSV_5000.csv"));
+            int sorszam = 1;
+            for (String line : lines) {
+                if (sorszam != 1) {
+                    String[] fields = line.split(",");
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    Dolgozo dolgozo = new Dolgozo(
+                            Integer.parseInt(fields[0]),
+                            fields[1],
+                            fields[2],
+                            fields[3],
+                            fields[4],
+                            Integer.parseInt(fields[5]),
+                            sdf.parse(fields[6]),
+                            Integer.parseInt(fields[7])
+                    );
+                    dolgozok.add(dolgozo);
                     System.out.println("");
                 }
-                sorszam.increment();
-            });
-
+                sorszam++;
+            }
 
             System.out.println("");
-        } catch (IOException e){
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
