f0b97d4c17eaf0315bdca774749e5128df442405
tenkesnet <rbardo@gmail.com>
2023-09-02 13:08:16 +0200

    Json előkészítése javítás

--- java_alapok/oopalapok/src/alapozo/JsonWrite.java
+++ java_alapok/oopalapok/src/alapozo/JsonWrite.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class JsonWrite {
     User user = new User(
             1,"Leanne Graham","Bret","Sincere@april.biz","1-770-736-8031 x56442","hildegard.org",
-            new Address("Kulas Light","Apt. 556","92998-3874","Gwenborough",new Geo(-37.3159,,81.1496),
-            new Company("Romaguera-Crona","Multi-layered client-server neural-net","harness real-time e-markets"))
+            new Address("Kulas Light","Apt. 556","92998-3874","Gwenborough",new Geo(-37.3159,81.1496)),
+            new Company("Romaguera-Crona","Multi-layered client-server neural-net","harness real-time e-markets")
     );
 
 }
