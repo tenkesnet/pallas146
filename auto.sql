7249e3f9ed5d421bf0d61136d93ed12b9771e857
tenkesnet <rbardo@gmail.com>
2023-09-30 09:34:26 +0200

    sz_autofelertekeles,sz_auto

--- auto.sql
+++ auto.sql
@@ -15,3 +15,25 @@ leiras text
 alter table sz_autotipus add constraint FK_sz_autotipus_sz_automarka foreign key (marka) references sz_automarka(nev) on update set null on delete set null;
 --alter table sz_autotipus drop constraint FK_sz_autotipus_sz_automarka;
 
+create table sz_auto (
+azon serial primary key,
+szin text,
+elso_vasarlas_idopontja date,
+elso_vasarlasi_ar numeric(20),
+tipus_azon int,
+rendszam text
+);
+
+alter table sz_auto add constraint FK_sz_autotipus_sz_auto foreign key (tipus_azon) references sz_autotipus(azon) on update set null on delete set null;
+--alter table sz_auto drop constraint FK_sz_autotipus_sz_auto;
+
+create table sz_autofelertekeles (
+auto_azon int,
+datum date,
+ertek numeric(20),
+primary key(auto_azon,datum)
+);
+
+alter table sz_autofelertekeles add constraint FK_sz_autofelertekeles_sz_auto foreign key (auto_azon) references sz_auto(azon) on update set null on delete set null;
+--alter table sz_autofelertekeles drop constraint FK_sz_autofelertekeles_sz_auto;
+
