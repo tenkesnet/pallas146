2c7ed63f68655be1e5aa9450b3b659a39688e79f
tenkesnet <rbardo@gmail.com>
2024-01-06 10:42:13 +0100

    Configuration changes

---
+++ backend/alaprest/src/main/java/org/pallas/alaprest/MyConfiguration.java
@@ -0,0 +1,12 @@
+package org.pallas.alaprest;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MyConfiguration {
+
+
+}
