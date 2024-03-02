42302ffe8d529214d3195298d389579bed3d88d5
tenkesnet <rbardo@gmail.com>
2024-03-02 10:47:41 +0100

    Test1 orai munka a nonnull referenciákra - javítás

--- backend/alaprest/src/test/java/org/pallas/alaprest/mappers/AlkalmazottRequestMapperTest.java
+++ backend/alaprest/src/test/java/org/pallas/alaprest/mappers/AlkalmazottRequestMapperTest.java
@@ -1,10 +1,17 @@
 package org.pallas.alaprest.mappers;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pallas.alaprest.dtos.AlkalmazottDTO;
 import org.pallas.alaprest.model.Alkalmazott;
+import org.pallas.alaprest.model.Autok;
+import org.pallas.alaprest.model.Reszleg;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,27 +40,25 @@ class AlkalmazottRequestMapperTest {
     @Test
     public void alkalmazottToDTOTest_AllNonNullReferences() {
         //Given
+        Reszleg reszleg = new Reszleg();
+        reszleg.setReszlegNev("Központ");
+        List<Autok> autok = new ArrayList<>();
         Alkalmazott alkalmazott = new Alkalmazott(
                 1,
                 23,
                 "Róbert",
-                "főnök",23000,4000,null,null,null);
-
+                "főnök",23000,4000, LocalDate.parse("2016-02-13"),
+                reszleg,
+                autok);
         //When
         AlkalmazottDTO alkalmazottDTO = AlkalmazottRequestMapper.MAPPER.fromAlkalmazott(alkalmazott);
-
         //Then
         assertEquals(alkalmazott.getAlkNev(), alkalmazottDTO.getNev());
         assertEquals(alkalmazott.getBeosztas(), alkalmazottDTO.getBeosztas());
-        assertEquals(null, alkalmazottDTO.getReszlegNev());
+        assertEquals(alkalmazott.getReszleg().getReszlegNev(), alkalmazottDTO.getReszlegNev());
         assertEquals(alkalmazott.getFizetes(), alkalmazottDTO.getFizetes());
         assertEquals(alkalmazott.getPremium(), alkalmazottDTO.getPremium());
-        assertEquals(alkalmazott.getBelepes(), null);
-
-    }
-
-    @Test
-    void testFromAlkalmazott() {
+        assertEquals(alkalmazott.getBelepes(), LocalDate.of(2016,2,13));
     }
 
     @Test
